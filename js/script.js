01150eece62ac76f62b7728bcfd6a79181d88519
kamdarnishit <kamdarnishit@gmail.com>
2016-03-21 09:37:53 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+(function ($) {
+Drupal.behaviors.myBehavior = {
+  attach: function (context, settings) {
+    
+        var inputData = settings.views.animateSlider;
+
+        console.log(inputData);
+
+        var field_labels = inputData.field_labels;
+
+        var outputData = {};
+        
+        var id=[];
+        
+        var details={};
+        
+        for(var i=0;i<inputData.field_labels.length;i++){
+            id='#animate-'+field_labels[i];
+            var transitionData = {show:inputData.transitions[field_labels[i]].$showTransition,hide:inputData.transitions[field_labels[i]].$hideTransition,delayShow : "delay1s"}
+            details[id]=transitionData;
+    }
+
+         console.log(details);   
+            
+        for(var i=0;i<inputData.count_rows;i++){
+            outputData[i] = details;
+        }
+        
+   $(".anim-slider").animateSlider(
+		 	{
+		 		autoplay	:inputData.$autoplay,
+		 		interval	:inputData.$Interval,
+		 		animations 	: outputData,
+		 	});
+  }
+};
+})(jQuery);
